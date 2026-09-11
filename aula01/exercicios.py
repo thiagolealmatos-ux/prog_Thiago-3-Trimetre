6e1ab75094e285bf4d3e4cf8e4675e9ff77671c2
thiagolealmatos-ux <thiago.leal.matos@escola.pr.gov.br>
2026-09-11 15:28:21 -0300

    Alteração em todas as funções por conta de estar dando erro

--- aula01/exercicios.py
+++ aula01/exercicios.py
@@ -7,55 +7,55 @@ Escreva sua solucao no lugar do 'pass'.
 
 def soma_lista(lista):
     soma = 0
-  for n in lista:
-    soma = soma + n
-  return soma
+    for n in lista:
+        soma = soma + n
+    return soma
 
 
 def conta_pares(lista):
-  contador = 0
-  for n in lista:
-    if n%2 == 0:
-      contador = contador + 1
-  return contador
+    contador = 0
+    for n in lista:
+        if n % 2 == 0:
+            contador = contador + 1
+    return contador
+
 
 def maior_valor(lista):
-  maior = lista[0]
-  for n in lista:
-    if n > maior:
-      maior = n
-  return maior
-    
+    maior = lista[0]
+    for n in lista:
+        if n > maior:
+            maior = n
+    return maior
+
 
 def existe(lista, alvo):
     for i in lista:
         if i == alvo:
             return True
-      
-      return False
+    return False
 
 
 def busca_linear(lista, alvo):
-   for i in range (len ( lista ) ) :
-      if lista [ i ] == alvo :
-          return i 
-   return -1
+    for i in range(len(lista)):
+        if lista[i] == alvo:
+            return i
+    return -1
 
 
 def segundo_maior(lista):
-  maior = lista[0]
-  segundo = lista[1]
-  
-  if maior < segundo:
-      temp = maior
-      maior = segundo
-      segundo = temp
+    maior = lista[0]
+    segundo = lista[1]
 
-  for i in lista[2:]:
-      if i > maior:
-       segundo = maior
-       maior = i
+    if maior < segundo:
+        temp = maior
+        maior = segundo
+        segundo = temp
 
-      elif i > segundo:
-        segundo = i
-  return segundo
+    for i in lista[2:]:
+        if i > maior:
+            segundo = maior
+            maior = i
+        elif i > segundo:
+            segundo = i
+
+    return segundo
