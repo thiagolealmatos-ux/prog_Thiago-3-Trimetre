e2d784e51f3a704346d263556dc3af8e6c3056ef
thiagolealmatos-ux <thiago.leal.matos@escola.pr.gov.br>
2026-09-21 19:32:06 -0300

    Resolvendo exercicios aula 2

--- aula02/exercicios.py
+++ aula02/exercicios.py
@@ -6,28 +6,57 @@ Escreva sua solucao no lugar do 'pass'.
 
 
 def remove_negativos(lista):
-    """Devolve uma lista nova so com os numeros que nao sao negativos."""
-    pass
+  
+    nova_lista = []
+    for numero in lista:
+        if numero >= 0:
+            nova_lista.append(numero)
+
+    return nova_lista
 
 
 def inverte(lista):
-    """Devolve uma lista nova na ordem contraria.
-    Sem usar reverse() e sem usar [::-1]."""
-    pass
+    
+    nova_lista = []
+    for i in range(len(lista) - 1, -1, -1):
+        nova_lista.append(lista[i])
+
+    return nova_lista
 
 
 def busca_binaria(lista, alvo):
-    """Recebe uma lista JA ORDENADA. Devolve a posicao do alvo, ou -1."""
-    pass
+  
+    inicio = 0
+    fim = len(lista) - 1
+
+    while inicio <= fim:
+        meio = (inicio + fim) // 2
+
+        if lista[meio] == alvo:
+            return meio
+        elif lista[meio] < alvo:
+            inicio = meio + 1
+        else:
+            fim = meio - 1
+
+    return -1
 
 
 def intercala(lista_a, lista_b):
-    """Devolve uma lista nova alternando os elementos das duas.
-    As duas listas tem o mesmo tamanho."""
-    pass
+
+    nova_lista = []
+    for i in range(len(lista_a)):
+        nova_lista.append(lista_a[i])
+        nova_lista.append(lista_b[i])
+
+    return nova_lista
 
 
 def remove_repetidos(lista):
-    """(Desafio) Devolve uma lista nova sem repetidos,
-    mantendo a ordem da primeira aparicao."""
-    pass
+
+    nova_lista = []
+    for numero in lista:
+        if numero not in nova_lista:
+            nova_lista.append(numero)
+
+    return nova_lista
